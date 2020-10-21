9c46b74dfc90a77cf49ddad29eee202e09bfa04a
Miles Ingram <milesingrams@gmail.com>
2020-10-21 17:04:03 -0400

    Small html tweaks

--- app/src/App.js
+++ app/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      Hello World!
+    <div className="app-wrap">
+      <h1 className="chat-title">Chat Room</h1>
+      <div className="messages">
+      </div>
+      <input type="text" />
     </div>
   );
 }
